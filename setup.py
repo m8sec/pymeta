d8d65d5e33a15da4eba9d1da05a6bb7f832263fe
m8r0wn <m8r0wn@protonmail.com>
2022-09-17 22:12:43 -0400

    v1.2.0 update

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='pymetadata',
-    version='1.2.0',
-    author='m8sec',
+    name='pymetasec',
+    version='1.1.1',
+    author= 'm8sec',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/m8r0wn/pymeta',
+    url='https://github.com/m8sec/pymeta',
     license='GPLv3',
+    platforms=["Unix"],
     packages=find_packages(include=["pymeta", "pymeta.*"]),
+    package_data={'pymeta': ['resources/*']},
     install_requires=[
         'requests>=2.28.1',
         'bs4>=0.0.1'
@@ -22,7 +24,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Topic :: Security"
     ],
-    entry_points= {
-        'console_scripts': ['pymeta=pymeta:main', 'pymetadata=pymeta:main']
+    entry_points={
+        'console_scripts': ['pymeta=pymeta:main', 'pymetasec=pymeta:main']
     }
 )
