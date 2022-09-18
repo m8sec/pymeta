948a85dbf0f525d9ba7b5f4c4d0655ead290d9b4
Mike <13889819+m8sec@users.noreply.github.com>
2022-09-17 21:53:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pymetadata',
-    version='1.1.0',
+    version='1.1.1',
     author = 'm8sec',
     description = 'Web scraper to download and extract file metadata',
     long_description=long_description,
